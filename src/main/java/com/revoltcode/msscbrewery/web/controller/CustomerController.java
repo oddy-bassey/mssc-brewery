c9fa41991ad7f3930f97d261b4f87eb877c138a4
@revoltcode <oddybassey@gmail.com>
2021-01-15 01:34:42 +0100

    Created REST end points(DELETE, POST, PUT) for customers

--- src/main/java/com/revoltcode/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/com/revoltcode/msscbrewery/web/controller/CustomerController.java
@@ -1,14 +1,13 @@
 package com.revoltcode.msscbrewery.web.controller;
 
+import com.revoltcode.msscbrewery.web.model.BeerDto;
 import com.revoltcode.msscbrewery.web.model.CustomerDto;
 import com.revoltcode.msscbrewery.web.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -27,5 +26,32 @@ public class CustomerController {
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable  UUID customerId){
 
         return new ResponseEntity<CustomerDto>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody CustomerDto customerDto){
+
+        CustomerDto customer = customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders header = new HttpHeaders();
+
+        header.add("location", "api/v1/customer/"+customer.getId());
+
+        return new ResponseEntity(header, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity handlePut(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customerDto){
+
+        customerService.updateCustomer(customerId, customerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable("customerId") UUID customerId){
+
+        customerService.deleteCustomerById(customerId);
     }
 }
--- src/main/java/com/revoltcode/msscbrewery/web/services/CustomerServiceImpl.java
+++ src/main/java/com/revoltcode/msscbrewery/web/services/CustomerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.revoltcode.msscbrewery.web.services;
 
 import com.revoltcode.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class CustomerServiceImpl implements CustomerService{
 
     @Override
@@ -14,5 +16,23 @@ public class CustomerServiceImpl implements CustomerService{
                 .id(UUID.randomUUID())
                 .name("Emmanuel Richards")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name(customerDto.getName())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo impl - would add a real implementation to update beer
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+        log.debug("deleting customer "+customerId);
     }
 }
