2325463b89a272dd3363b89f96c4882f3027aa65
@revoltcode <oddybassey@gmail.com>
2021-02-10 15:02:16 +0100

    Implementing method Validation. closes #3

--- src/main/java/com/revoltcode/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/com/revoltcode/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -6,14 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Validated
 @RequestMapping("/api/v2/beer")
 @RestController
 public class BeerControllerV2 {
@@ -25,13 +28,13 @@ public class BeerControllerV2 {
     }
 
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDtoV2> getBeer(@PathVariable("beerId") UUID beerId){
+    public ResponseEntity<BeerDtoV2> getBeer(@NotNull @PathVariable("beerId") UUID beerId){
 
         return new ResponseEntity<BeerDtoV2>(beerServiceV2.getBerById(beerId), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity handlePost(@Valid @RequestBody BeerDtoV2 beerDto){
+    public ResponseEntity handlePost(@Valid @NotNull @RequestBody BeerDtoV2 beerDto){
         BeerDtoV2 beer = beerServiceV2.saveNewBeer(beerDto);
 
         HttpHeaders header = new HttpHeaders();
