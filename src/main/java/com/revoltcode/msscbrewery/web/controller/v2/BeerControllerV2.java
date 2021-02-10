81fa97b55fad46854977ed68d0fe478907a063b7
@revoltcode <oddybassey@gmail.com>
2021-02-10 14:52:46 +0100

    Implementing Validation Error Handling. closes #2

--- src/main/java/com/revoltcode/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/com/revoltcode/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -8,6 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RequestMapping("/api/v2/beer")
@@ -27,7 +31,7 @@ public class BeerControllerV2 {
     }
 
     @PostMapping
-    public ResponseEntity handlePost(@RequestBody BeerDtoV2 beerDto){
+    public ResponseEntity handlePost(@Valid @RequestBody BeerDtoV2 beerDto){
         BeerDtoV2 beer = beerServiceV2.saveNewBeer(beerDto);
 
         HttpHeaders header = new HttpHeaders();
@@ -39,7 +43,7 @@ public class BeerControllerV2 {
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDtoV2 beerDto){
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDtoV2 beerDto){
 
         beerServiceV2.updateBeer(beerId, beerDto);
 
@@ -51,5 +55,17 @@ public class BeerControllerV2 {
     public void deleteBeer(@PathVariable("beerId") UUID beerId){
 
         beerServiceV2.deleteById(beerId);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List> validationErrorHandler(ConstraintViolationException ex){
+
+        List<String> errors = new ArrayList<>(ex.getConstraintViolations().size());
+
+        ex.getConstraintViolations().forEach(constraintViolation -> {
+            errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage());
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
